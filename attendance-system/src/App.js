5047d86d9cc55132c077045bc6e4ea3b0ebe4319
Tanner Jones <127117143+TannerJ0nes@users.noreply.github.com>
2024-02-22 09:31:18 -0330

    Update routing
    User sign in selection page removed and routing changed!

--- attendance-system/src/App.js
+++ attendance-system/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignInPage from './pages/SignInPage';
 import LoginPage from './pages/LoginPage';
 import StudentDashboardPage from './pages/StudentDashboardPage';
 import AdminDashboardPage from './pages/AdminDashboardPage';
@@ -10,8 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/student-dashboard" element={<StudentDashboardPage />} />
         <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
       </Routes>
