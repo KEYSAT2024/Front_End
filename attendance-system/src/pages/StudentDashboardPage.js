0d9550847d7bd622999535a4c9743cba39cd2d82
omeganave <omeganave@outlook.com>
2024-02-23 17:29:10 -0330

    Added react-icons package

--- attendance-system/src/pages/StudentDashboardPage.js
+++ attendance-system/src/pages/StudentDashboardPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
+import { BsPencilSquare } from "react-icons/bs";
 
 function StudentDashboardPage() {
   return (
     <div>
       <Header></Header>
       <Navbar>
+        <Link to='/student-dashboard' className='nav-link'>
+          <BsPencilSquare /> Attendance
+        </Link>
       </Navbar>
     </div>
   );
