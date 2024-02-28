b3ad3a674cc28fccf0dca9fe7a5b178c7ca6b4bd
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2024-02-28 10:39:41 -0330

    student dashboard css and get rid of console errors for using class instead of className

--- attendance-system/src/pages/StudentDashboardPage.js
+++ attendance-system/src/pages/StudentDashboardPage.js
@@ -1,26 +1,31 @@
-import React from "react";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
-import { BsPencilSquare } from "react-icons/bs";
-import { useAuth } from "../auth/auth";
+import React from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
+import { BsPencilSquare } from 'react-icons/bs';
+import { useAuth } from '../auth/auth';
 
 function StudentDashboardPage() {
   const auth = useAuth();
 
   return (
-    <div>
-      <Header></Header>
-      <Navbar>
-        <Link to="/student-dashboard" className="nav-link">
-          <BsPencilSquare /> Attendance
-        </Link>
-      </Navbar>
-      <div className="main-content">
-        <h2>Student Dashboard</h2>
-        <p>Welcome, {auth.user}!</p>
+    <>
+      <div className='container'>
+        <Header></Header>
+        <Navbar>
+          <Link
+            to='/student-dashboard'
+            className='nav-link'
+          >
+            <BsPencilSquare /> Attendance
+          </Link>
+        </Navbar>
+        <div className='main-content'>
+          <h2>Student Dashboard</h2>
+          <p>Welcome, {auth.user}!</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
